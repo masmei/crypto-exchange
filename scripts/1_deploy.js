6a465de69a090390cbe98cdf6a3fee71cc601feb
Mason Mei <masonmei@pursuit.org>
2023-04-17 01:14:29 -0500

    finished seedexchange script

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,19 +1,38 @@
-const hre = require("hardhat");
-
 async function main() {
+  console.log("Preparing deployment...\n")
+
   //Fetch contact to deploy
   const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  //Fetch accounts
+  const accounts = await ethers.getSigners()
+  console.log(`Accounts fetched: \n${accounts[0].address}\n${accounts[1].address}\n`)
 
   //Deploy contract
-  const token = await Token.deploy()
+  const monie = await Token.deploy("MONIE", "MONIE", "1000000")
+  await monie.deployed()
+  console.log(`MONIE Token deployed to ${monie.address}`)
 
-  await token.deployed()
-  console.log(`Token deployed to ${token.address}`)
+  const mDai = await Token.deploy("mDAI", "mDAI", "1000000")
+  await mDai.deployed()
+  console.log(`mDAI Token deployed to ${mDai.address}`)
+
+  const mEth = await Token.deploy("mETH", "mETH", "1000000")
+  await mEth.deployed()
+  console.log(`mETH Token deployed to ${mEth.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 5)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
