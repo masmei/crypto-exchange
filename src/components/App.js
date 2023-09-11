e3cadef4f2522b2f020ebee31ca1cf033d1a4f01
Mason Mei <masonmei@pursuit.org>
2023-09-11 01:28:51 -0400

    added redux for exchange & account balance

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,39 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import config from './config.json';
+import config from '../config.json';
 
 import {
   loadProvider,
   loadNetwork,
   loadAccount,
-  loadToken
-} from './store/interactions';
+  loadTokens,
+  loadExchange
+} from '../store/interactions';
 
 function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch)
-
     // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
+
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Token Smart Contract
-    await loadToken(provider, config[chainId].monie.address, dispatch)
+    // Fetch current account & balance from Metamask
+    await loadAccount(provider, dispatch)
+    
+    // Load Token Smart Contract
+    const monie = config[chainId].monie
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [monie.address, mETH.address], dispatch)
+    
+    // Load Exchange Smart Contract
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
   }
+
+
 
   useEffect(() => {
     loadBlockchainData()
