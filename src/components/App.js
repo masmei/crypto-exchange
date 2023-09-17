3e9f777903f4d63f87192a1c5d1155055f4ac6d7
Mason Mei <masonmei@pursuit.org>
2023-09-17 16:13:28 -0400

    added markets/balances

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
+import Markets from './Markets';
+import Balance from './Balance';
 
 function App() {
   const dispatch = useDispatch()
@@ -39,7 +42,10 @@ function App() {
 
     // Load exchange smart contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to Events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => {
@@ -54,9 +60,9 @@ function App() {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
